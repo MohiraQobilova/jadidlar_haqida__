30d84d82baed19d483cd06f18406f2b6158b5873
MohiraQobilova <139129851+MohiraQobilova@users.noreply.github.com>
2024-02-05 18:12:18 +0400

    nimadir

---
+++ java/main.js
@@ -0,0 +1,302 @@
+"use strict";
+
+function mOver(){
+    document.querySelector(".content").style.display="block";
+}
+
+
+
+function click(){
+    document.querySelector(".content").style.display="block";
+}
+
+function closeModal( ){
+    document.querySelector(".content").style.display = "none";
+}   
+
+
+
+function mOver1(){
+    document.querySelector(".content1").style.display="block";
+}
+
+function click(){
+    document.querySelector(".content1").style.display="block";
+}
+
+function closeModal1( ){
+    document.querySelector(".content1").style.display = "none";
+} 
+
+
+
+function mOver2(){
+    document.querySelector(".content2").style.display="block";
+}
+
+function click(){
+    document.querySelector(".content2").style.display="block";
+}
+
+function closeModal2( ){
+    document.querySelector(".content2").style.display = "none";
+}   
+
+
+function mOver3(){
+    document.querySelector(".content3").style.display="block";
+}
+
+
+function click(){
+    document.querySelector(".content3").style.display="block";
+}
+
+function closeModal3( ){
+    document.querySelector(".content3").style.display = "none";
+}  
+
+
+function mOver4(){
+    document.querySelector(".content4").style.display="block";
+}
+
+
+function click(){
+    document.querySelector(".content4").style.display="block";
+}
+
+function closeModal4( ){
+    document.querySelector(".content4").style.display = "none";
+}
+
+
+
+function mOver5(){
+    document.querySelector(".content5").style.display="block";
+}
+
+
+function click(){
+    document.querySelector(".content5").style.display="block";
+}
+
+function closeModal5( ){
+    document.querySelector(".content5").style.display = "none";
+}   
+
+function mOver6(){
+    document.querySelector(".content6").style.display="block";
+}
+
+
+function click(){
+    document.querySelector(".content6").style.display="block";
+}
+
+function closeModal6(){
+    document.querySelector(".content6").style.display = "none";
+}   
+
+function mOver7(){
+    document.querySelector(".content7").style.display="block";
+}
+
+
+function click(){
+    document.querySelector(".content7").style.display="block";
+}
+
+function closeModal7( ){
+    document.querySelector(".content7").style.display = "none";
+}   
+
+function mOver8(){
+    document.querySelector(".content8").style.display="block";
+}
+
+
+function click(){
+    document.querySelector(".content8").style.display="block";
+}
+
+function closeModal8( ){
+    document.querySelector(".content8").style.display = "none";
+}   
+
+function mOver9(){
+    document.querySelector(".content9").style.display="block";
+}
+
+
+function click(){
+    document.querySelector(".content9").style.display="block";
+}
+
+function closeModal9( ){
+    document.querySelector(".content9").style.display = "none";
+}   
+
+function mOver10(){
+    document.querySelector(".content10").style.display="block";
+}
+
+
+function click(){
+    document.querySelector(".content10").style.display="block";
+}
+
+function closeModal10( ){
+    document.querySelector(".content10").style.display = "none";
+   
+} 
+
+function mOver11(){
+    document.querySelector(".content11").style.display="block";
+}
+
+
+function click(){
+    document.querySelector(".content11").style.display="block";
+}
+
+function closeModal11( ){
+    document.querySelector(".content11").style.display = "none";
+}   
+
+function mOver12(){
+    document.querySelector(".content12").style.display="block";
+}
+
+
+function click(){
+    document.querySelector(".content12").style.display="block";
+}
+
+function closeModal12( ){
+    document.querySelector(".content12").style.display = "none";
+}   
+
+function mOver13(){
+    document.querySelector(".content13").style.display="block";
+}
+
+
+function click(){
+    document.querySelector(".content13").style.display="block";
+}
+
+function closeModal13( ){
+    document.querySelector(".content13").style.display = "none";
+}  
+
+
+function mOver14(){
+    document.querySelector(".content14").style.display="block";
+}
+
+
+function click(){
+    document.querySelector(".content14").style.display="block";
+}
+
+function closeModal14( ){
+    document.querySelector(".content14").style.display = "none";
+} 
+
+
+
+function mOver15(){
+    document.querySelector(".content15").style.display="block";
+}
+
+
+function click(){
+    document.querySelector(".content15").style.display="block";
+}
+
+function closeModal15( ){
+    document.querySelector(".content15").style.display = "none";
+}   
+
+function mOver16(){
+    document.querySelector(".content16").style.display="block";
+}
+
+
+function click(){
+    document.querySelector(".content16").style.display="block";
+}
+
+function closeModal16( ){
+    document.querySelector(".content16").style.display = "none";
+}  
+
+function mOver17(){
+    document.querySelector(".content17").style.display="block";
+}
+
+
+function click(){
+    document.querySelector(".content17").style.display="block";
+}
+
+function closeModal17( ){
+    document.querySelector(".content17").style.display = "none";
+}  
+
+function mOver18(){
+    document.querySelector(".content18").style.display="block";
+}
+
+
+function click(){
+    document.querySelector(".content18").style.display="block";
+}
+
+function closeModal18( ){
+    document.querySelector(".content18").style.display = "none";
+}   
+
+function mOver19(){
+    document.querySelector(".content19").style.display="block";
+}
+
+
+function click(){
+    document.querySelector(".content19").style.display="block";
+}
+
+function closeModal19( ){
+    document.querySelector(".content19").style.display = "none";
+}   
+
+
+
+
+
+function mOver20(){
+    document.querySelector(".content20").style.display="block";
+}
+
+
+function click(){
+    document.querySelector(".content20").style.display="block";
+}
+
+function closeModal20( ){
+    document.querySelector(".content20").style.display = "none";
+}   
+
+function mOve21(){
+    document.querySelector(".content21").style.display="block";
+}
+
+
+function click(){
+    document.querySelector(".content21").style.display="block";
+}
+
+function closeModal21( ){
+    document.querySelector(".content21").style.display = "none";
+}   
